a53f500b5273e569d10180f5718c4932f5b53d77
Mark Halls <mark.halls@gmail.com>
2019-10-17 13:36:51 -0700

    Add basic form validation

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,24 +1,30 @@
 import React from "react";
 import { Form, Field, withFormik } from "formik";
+import * as Yup from "yup";
 
-const UserForm = ({ values }) => {
+const UserForm = ({ values, touched, errors }) => {
   return (
     <Form>
       <Field type="text" name="name" placeholder="Name" value={values.name} />
+      {touched.name && errors.name && <p>{errors.name}</p>}
       <Field
         type="email"
         name="email"
         placeholder="Email"
         value={values.email}
       />
+      {touched.email && errors.email && <p>{errors.email}</p>}
+
       <Field
         type="password"
         name="password"
         placeholder="Password"
         value={values.password}
       />
+      {touched.password && errors.password && <p>{errors.password}</p>}
+
       <label>
-        <Field component="checkbox" name="terms" />" I agree to the terms and
+        <Field type="checkbox" name="terms" />" I agree to the terms and
         conditions."
       </label>
       <button type="submit">Submit</button>
@@ -34,7 +40,12 @@ const FormikUserForm = withFormik({
       password: password || "",
       terms: terms || false
     };
-  }
+  },
+  validationSchema: Yup.object().shape({
+    name: Yup.string().required("Name is required."),
+    email: Yup.string().required(),
+    password: Yup.string().required()
+  })
 })(UserForm);
 
 export default FormikUserForm;
